0c6f056b017cd48c86a81465d9aa4639da95bb4e
Linus <linus.nordin@gmail.com>
2019-06-29 15:30:09 +0200

    Update fix_updates.sh
    more logging

--- deconz/fix_updates.sh
+++ deconz/fix_updates.sh
@@ -14,10 +14,14 @@
 
 # add this to crontab, edit paths if needed
 # 0 5 * * * /usr/bin/python2 /usr/share/hassio/addons/data/69bb46cb_deconz/otau/ikea-ota-download.py > /usr/share/hassio/addons/data/69bb46cb_deconz/otau/update_download.log 2>&1
-# 2 5 * * * /usr/share/hassio/addons/data/69bb46cb_deconz/otau/fix_updates.sh     > /usr/share/hassio/addons/data/69bb46cb_deconz/otau/update_restart.log 2>&1
+# 2 5 * * * /usr/share/hassio/addons/data/69bb46cb_deconz/otau/fix_updates.sh     >> /usr/share/hassio/addons/data/69bb46cb_deconz/otau/update_restart.log 2>&1
 
+LOG=/usr/share/hassio/addons/data/69bb46cb_deconz/otau/restart_deconz.log
+date | tee -a $LOG
 if [[ `grep -v 'already' /usr/share/hassio/addons/data/69bb46cb_deconz/otau/update_download.log` != "" ]]; then
-	echo "updates found, restarting deconz..."
-	/usr/local/bin/hass-cli service call hassio.addon_restart --arguments addon=69bb46cb_deconz
+        echo "updates found, restarting deconz..." | tee -a $LOG
+        /usr/local/bin/hass-cli service call hassio.addon_restart --arguments addon=69bb46cb_deconz | tee -a $LOG
+else
+        echo "no new updates found, nothing to do" | tee -a $LOG
 fi
 
